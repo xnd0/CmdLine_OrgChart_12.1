f0c877f64c514876852cbbcb3f93f2b14d704b74
xndcmd <xndcmd@gmail.com>
2022-07-01 08:01:30 -0700

    added server.js + express, mysql pac kages

---
+++ server.js
@@ -0,0 +1,10 @@
+// --------------------- //
+// ----- server.js ----- //
+// --------------------- //
+
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+

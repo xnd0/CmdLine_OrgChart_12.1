bdc0c590b73c187fc97c8773e44d490aa8a6e6e2
xndcmd <xndcmd@gmail.com>
2022-07-01 08:57:02 -0700

    fixed import bug. Inquirer successfully launches

--- server.js
+++ server.js
@@ -2,9 +2,55 @@
 // ----- server.js ----- //
 // --------------------- //
 
-const express = require('express');
-const mysql = require('mysql2');
+// const express = require('express');
+import express from 'express';
+// var inquirer = require('inquirer');
+import inquirer from 'inquirer';
+// const mysql = require('mysql2');
+import mysql from 'mysql2';
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
+
+
+function init() {
+    // Need the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is your name?',
+            },
+            {
+                type: 'checkbox',
+                message: 'What languages do you know?',
+                name: 'stack',
+                choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+            },
+            {
+                type: 'list',
+                message: 'What is your preferred method of communication?',
+                name: 'contact',
+                choices: ['email', 'phone', 'telekinesis'],
+            },
+        ])
+        .then((data) => {
+            const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+
+            fs.writeFile(filename, generateHTML(data), (err) =>
+                err ? console.log(err) : console.log('Success!')
+            );
+        });
+
+}
+// ^^ end init() function ^^ //
+
+
+
+
+
+init();

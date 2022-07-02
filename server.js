0ee034cf38e6e8522449d2fc298464f0adc27b25
xndcmd <xndcmd@gmail.com>
2022-07-01 18:40:22 -0700

    successfully displays department names and ids in table format in console

--- server.js
+++ server.js
@@ -2,19 +2,41 @@
 // ----- server.js ----- //
 // --------------------- //
 
-// const express = require('express');
+
 import express from 'express';
-// var inquirer = require('inquirer');
+
 import inquirer from 'inquirer';
-// const mysql = require('mysql2');
+
 import mysql from 'mysql2';
 
-// import db from './config/connection.js';
+import consoleTable from 'console.table';
 
-const PORT = process.env.PORT || 3001;
+// import sequelize from 'sequelize';
+// import {sequelize} from '../config/connection.js';
+// import connection from '../CmdLine_OrgChart_12.1/config/connection.js';
+
+// const PORT = process.env.PORT || 3001;
 const app = express();
 
+// const db = mysql.createConnection(
+//     {
+//         host: "localhost",
+//         user: process.env.DB_USER,
+//         password: process.env.DB_PASSWORD,
+//         database: process.env.DB_NAME
+//     },
+//     console.log(`connected to the databse`)
+// );
+// console.log(db);
 
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    // MySQL info,
+    user: 'root',
+    password: 'potato22',
+    database: 'user_db'
+})
 
 
 function init() {
@@ -57,9 +79,21 @@ function init() {
 
         });    
         
-        // View Departments choice function
+        // formatted table showing department names and department ids
         function viewDepartments() {
             console.log('viewDepartments function works');
+
+            const sqlInput = `SELECT * FROM department`;
+
+            // console.log(sqlInput);
+            db.query(sqlInput, (err, data) => {
+                if (err) {
+                    console.log(err);
+                }
+                console.table(data);
+            });
+
+
         }
 
 };

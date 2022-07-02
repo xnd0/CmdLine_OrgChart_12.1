b1881ac6aa29c513eaeb4156c5261346ba4f5625
xndcmd <xndcmd@gmail.com>
2022-07-02 09:12:28 -0700

    updated viewEmployees function to display all required table columns

--- server.js
+++ server.js
@@ -129,7 +129,13 @@ function init() {
     function viewEmployees() {
         console.log('viewEmployees function works');
         // const sqlInput = `SELECT * FROM employee`;
-        const sqlInput = `SELECT * FROM employee`;
+        const sqlInput = `
+            SELECT department.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, department.name AS manager
+            FROM employee 
+            LEFT JOIN employee manager ON manager.id = employee.manager_id 
+            INNER JOIN role ON (role.id = employee.role_id) 
+            INNER JOIN department ON (department.id = role.department_id)`
+            ;
 
         db.query(sqlInput, (err, data) => {
             if (err) {
